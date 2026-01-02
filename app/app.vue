76132ab68bbaa28bb6808901b5ab5f5b9eb364e4
ogolknev <ogolknevnv@ya.ru>
2026-01-02 11:52:03 +0300

    Add footer

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,6 @@
     <div class="w-dvw h-dvh bg-bg">
         <AppHeader />
         <NuxtPage />
+        <AppFooter />
     </div>
 </template>
---
+++ app/components/AppFooter.vue
@@ -0,0 +1,82 @@
+<template>
+    <footer
+        :class="[
+            'h-168 px-25 pt-13 pb-8 bg-dark-bg text-fg text-[1.25rem]',
+            'flex flex-col justify-between',
+        ]">
+        <div class="flex">
+            <nav>
+                <ul class="flex flex-col gap-8">
+                    <li v-for="item in navItems">
+                        <NuxtLink :to="item.to">{{ item.label }}</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+
+            <ul class="flex flex-col gap-8 ml-42">
+                <li v-for="item in legalItems">
+                    <NuxtLink :to="item.to">{{ item.label }}</NuxtLink>
+                </li>
+            </ul>
+
+            <ul class="flex flex-col gap-8 ml-100">
+                <li v-for="item in socialItems">
+                    <NuxtLink :to="item.to">{{ item.label }}</NuxtLink>
+                </li>
+            </ul>
+        </div>
+
+        <div class="flex gap-5 items-end">
+            <img class="w-170" src="/footer-logo-0.svg" alt="">
+            <img class="w-135" src="/footer-logo-1.svg" alt="">
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+type NavItem = {
+    label: string;
+    to: string;
+};
+
+const navItems: NavItem[] = [
+    {
+        label: "Главная",
+        to: "/",
+    },
+    {
+        label: "Товары",
+        to: "/",
+    },
+    {
+        label: "О компании",
+        to: "/",
+    },
+    {
+        label: "Контакты",
+        to: "/",
+    },
+];
+
+const legalItems: NavItem[] = [
+    {
+        label: "Договор оферты",
+        to: "/",
+    },
+    {
+        label: "Политика конфиденциальности",
+        to: "/",
+    },
+];
+
+const socialItems: NavItem[] = [
+    {
+        label: "Telegram",
+        to: "/",
+    },
+    {
+        label: "IG",
+        to: "/",
+    },
+];
+</script>
